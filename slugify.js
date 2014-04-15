e6d25e2ae577c0ae855a2b291cf81a7b351b4bbc
Andre Farzat <andrefarzat@gmail.com>
2014-04-15 11:33:35 -0400

    transforming into a AMD package

--- slugify.js
+++ slugify.js
@@ -1,15 +1,32 @@
-String.prototype.slugify = function(){
-    var from = 'àáäãâèéëêìíïîòóöôõùúüûñç·/_,:;',
-        to = 'aaaaaeeeeiiiiooooouuuunc------',
-        i = 0,
-        len = from.length,
-        str = this.toLowerCase();
+(function (root, factory) {
 
-    for( ; i < len; i++ ){
-        str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define([root], factory);
+    } else {
+        // Browser globals
+        root.slugify = factory(root);
     }
 
-    return str.replace(/^\s+|\s+$/g, '') //trim
-        .replace(/[^-a-zA-Z0-9\s]+/ig, '')
-        .replace(/\s/gi, "-");
-};
+}(this, function (window) {
+
+    var from = 'àáäãâèéëêìíïîòóöôõùúüûñç·/_,:;',
+        to = 'aaaaaeeeeiiiiooooouuuunc------';
+
+    return function slugify(str){
+        var i = 0,
+            len = from.length;
+        
+        str = str.toLowerCase();
+
+        for( ; i < len; i++ ){
+            str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+        }
+
+        return str.replace(/^\s+|\s+$/g, '') //trim
+            .replace(/[^-a-zA-Z0-9\s]+/ig, '')
+            .replace(/\s/gi, "-");
+    };
+
+
+}));
